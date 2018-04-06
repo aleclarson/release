1a875da2e955b4bcc4e8d7c4bb56d9d229358e89
aleclarson <alec.stanford.larson@gmail.com>
2018-04-06 14:10:55 -0400

    fix: abort publish if an npm script fails

--- publish.sh
+++ publish.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+set -e
 
 # Prepare the package for its release, and amend the bump commit.
-npm run prepublish -s --if-present &&
-npm run prepare -s --if-present &&
-npm run prepublishOnly -s --if-present &&
+npm run prepublish -s --if-present
+npm run prepare -s --if-present
+npm run prepublishOnly -s --if-present
 
 git add -A
 if ! [ -z "$(git status --porcelain)" ]; then
@@ -10,6 +12,6 @@ if ! [ -z "$(git status --porcelain)" ]; then
 fi
 
 # Create a tag for the version, and push it.
-git tag "$1" &&
-git push -u "$2" "$3" --porcelain -f &&
+git tag "$1"
+git push -u "$2" "$3" --porcelain -f
 git push "$2" "$1" --porcelain -f
