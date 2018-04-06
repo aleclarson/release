7540983a0dbcd7b437c3caa427c5dd550c705d33
aleclarson <alec.stanford.larson@gmail.com>
2018-04-06 13:59:57 -0400

    feat: run 'prepublish' and 'prepare' scripts

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 
 # Prepare the package for its release, and amend the bump commit.
+npm run prepublish -s &&
+npm run prepare -s &&
 npm run prepublishOnly -s &&
 git add -A &&
 git commit --amend --no-edit
