f59421240ecec2abc8667bcd134e9ca9f4607bbe
Alec Larson <alec.stanford.larson@gmail.com>
2019-03-10 14:54:04 -0400

    fix: only run "build" and "prepublish" scripts
    The "prepublishOnly" script is meant for "npm publish" only.
    
    The "prepare" script is meant for "git clone" workflows.

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-# Prepare the package for its release, and amend the bump commit.
 npm run build -s --if-present
 npm run prepublish -s --if-present
-npm run prepare -s --if-present
-npm run prepublishOnly -s --if-present
 
 git rm -r --cached .
 git add -A
+
+# Amend the bump commit if necessary.
 if ! [ -z "$(git status --porcelain)" ]; then
   git commit --amend --no-edit
 fi
